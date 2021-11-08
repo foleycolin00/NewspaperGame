c1f921a45f3e512c7b61b079d1261d8dc30196d8
elinewman12 <elis.newman12@gmail.com>
2021-11-07 23:07:57 -0500

    Added opponent logic
    Added logic to opponents so they focus on a given goal

--- NewspaperGame/ClientApp/src/classes/opponent.js
+++ NewspaperGame/ClientApp/src/classes/opponent.js
@@ -3,47 +3,51 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Opponent = void 0;
 var opponents_component_1 = require("../app/opponents/opponents.component");
 var newspaper_1 = require("./newspaper");
-var public_1 = require("./public");
 var tools_1 = require("./tools");
 var Opponent = /** @class */ (function () {
-    function Opponent(name) {
+    function Opponent(name, focus) {
         //Percentage Changes
         this.shiftSliderPercentage = 1;
         this.popDelta = 0;
         //Sliders
+        //Index explanation:
+        //0 = Journalism, 1 = Content, 2 = Tone, 3 = Sensationalism
+        //4 = Nationalization, 5 = Syndication
         this.sliders = [50, 50, 50, 50, 50, 50];
-        //Ballance - Ranges Will Allow
+        //Balance - Ranges Will Allow
         this.slidersLeft = [0, 0, 0, 0, 0, 0];
         this.slidersRight = [100, 100, 100, 100, 100, 100];
-        //Ballance - How Quick to Move
+        //Balance - How Quick to Move
         this.slidersDeltaLeft = [5, 5, 5, 5, 5, 5];
         this.slidersDeltaRight = [5, 5, 5, 5, 5, 5];
         this.name = name;
+        //Same explanation as slider index
+        this.focus = focus;
     }
     Opponent.generateOpponents = function () {
         var opps = [];
         var o;
         //Opponent 1
         //Classic local
-        o = new Opponent("Opponent 1");
+        o = new Opponent("Opponent 1", 0);
         o.popularity = 10;
         o.sliders = [50, 50, 50, 50, 50, 50];
         opps.push(o);
         //Opponent 1
         //Tabloid
-        o = new Opponent("Opponent 2");
+        o = new Opponent("Opponent 2", 3);
         o.popularity = 15;
         o.sliders = [10, 0, 75, 100, 50, 0];
         opps.push(o);
         //Opponent 1
         //Negative National
-        o = new Opponent("Opponent 3");
+        o = new Opponent("Opponent 3", 4);
         o.popularity = 5;
         o.sliders = [25, 75, 0, 75, 75, 15];
         opps.push(o);
         //Opponent 1
         //Really good national
-        o = new Opponent("Opponent 4");
+        o = new Opponent("Opponent 4", 1);
         o.popularity = 40;
         o.sliders = [100, 100, 0, 50, 100, 0];
         opps.push(o);
@@ -55,19 +59,15 @@ var Opponent = /** @class */ (function () {
     };
     Opponent.prototype.shiftSliders = function () {
         for (var i = 0; i < this.sliders.length; i++) {
-            //Will shift this slider randomly based on shiftSliderPercentage
-            if (Math.random() < this.shiftSliderPercentage) {
-                //Randomly shift left vs right
-                //Might change this later to "smarter" AI
-                if (this.sliders[i] > public_1.Public.slidersRight[i]) {
-                    if (this.sliders[i] - this.slidersDeltaLeft[i] >= this.slidersLeft[i]) {
-                        this.sliders[i] = tools_1.Tools.TrimNumber(this.sliders[i] - this.slidersDeltaLeft[i]);
-                    }
+            //Shifts the focus to the right and everything else to the left
+            if (this.focus == i) {
+                if (this.sliders[i] + this.slidersDeltaRight[i] >= this.slidersRight[i]) {
+                    this.sliders[i] = tools_1.Tools.TrimNumber(this.sliders[i] + this.slidersDeltaRight[i] * 2);
                 }
-                else if (this.sliders[i] < public_1.Public.slidersLeft[i]) {
-                    if (this.sliders[i] + this.slidersDeltaRight[i] >= this.slidersRight[i]) {
-                        this.sliders[i] = tools_1.Tools.TrimNumber(this.sliders[i] + this.slidersDeltaRight[i]);
-                    }
+            }
+            else {
+                if (this.sliders[i] - this.slidersDeltaLeft[i] >= this.slidersLeft[i]) {
+                    this.sliders[i] = tools_1.Tools.TrimNumber(this.sliders[i] - this.slidersDeltaLeft[i]);
                 }
             }
         }
--- NewspaperGame/ClientApp/src/classes/opponent.ts
+++ NewspaperGame/ClientApp/src/classes/opponent.ts
@@ -9,23 +9,29 @@ export class Opponent {
 
   //Info
   name: string;
+  focus: number;
   popularity: number;
   popDelta: number = 0;
   score: string;
 
   //Sliders
+  //Index explanation:
+  //0 = Journalism, 1 = Content, 2 = Tone, 3 = Sensationalism
+  //4 = Nationalization, 5 = Syndication
   sliders: number[] = [50, 50, 50, 50, 50, 50];
 
-  //Ballance - Ranges Will Allow
+  //Balance - Ranges Will Allow
   slidersLeft: number[] = [0, 0, 0, 0, 0, 0];
   slidersRight: number[] = [100, 100, 100, 100, 100, 100];
 
-  //Ballance - How Quick to Move
+  //Balance - How Quick to Move
   slidersDeltaLeft: number[] = [5, 5, 5, 5, 5, 5];
   slidersDeltaRight: number[] = [5, 5, 5, 5, 5, 5];
 
-  constructor(name) {
+  constructor(name, focus) {
     this.name = name
+    //Same explanation as slider index
+    this.focus = focus
   }
 
   static generateOpponents(): Opponent[]{
@@ -34,28 +40,28 @@ export class Opponent {
 
     //Opponent 1
     //Classic local
-    o = new Opponent("Opponent 1");
+    o = new Opponent("Opponent 1", 0);
     o.popularity = 10;
     o.sliders = [50, 50, 50, 50, 50, 50];
     opps.push(o)
 
     //Opponent 1
     //Tabloid
-    o = new Opponent("Opponent 2");
+    o = new Opponent("Opponent 2", 3);
     o.popularity = 15;
     o.sliders = [10, 0, 75, 100, 50, 0];
     opps.push(o)
 
     //Opponent 1
     //Negative National
-    o = new Opponent("Opponent 3");
+    o = new Opponent("Opponent 3", 4);
     o.popularity = 5;
     o.sliders = [25, 75, 0, 75, 75,15];
     opps.push(o)
 
     //Opponent 1
     //Really good national
-    o = new Opponent("Opponent 4");
+    o = new Opponent("Opponent 4", 1);
     o.popularity = 40;
     o.sliders = [100, 100, 0, 50, 100,0];
     opps.push(o)
@@ -71,20 +77,17 @@ export class Opponent {
 
   shiftSliders(): void {
     for (let i = 0; i < this.sliders.length; i++) {
-      //Will shift this slider randomly based on shiftSliderPercentage
-      if (Math.random() < this.shiftSliderPercentage) {
-        //Randomly shift left vs right
-        //Might change this later to "smarter" AI
-        if (this.sliders[i] > Public.slidersRight[i]) {
-          if (this.sliders[i] - this.slidersDeltaLeft[i] >= this.slidersLeft[i]) {
-            this.sliders[i] = Tools.TrimNumber(this.sliders[i] - this.slidersDeltaLeft[i]);
-          }
-        } else if (this.sliders[i] < Public.slidersLeft[i]){
-          if (this.sliders[i] + this.slidersDeltaRight[i] >= this.slidersRight[i]) {
-            this.sliders[i] = Tools.TrimNumber(this.sliders[i] + this.slidersDeltaRight[i]);
-          }
+      //Shifts the focus to the right and everything else to the left
+      if (this.focus == i) {
+        if (this.sliders[i] + this.slidersDeltaRight[i] >= this.slidersRight[i]) {
+          this.sliders[i] = Tools.TrimNumber(this.sliders[i] + this.slidersDeltaRight[i] * 2);
+        }
+      } else {
+        if (this.sliders[i] - this.slidersDeltaLeft[i] >= this.slidersLeft[i]) {
+          this.sliders[i] = Tools.TrimNumber(this.sliders[i] - this.slidersDeltaLeft[i]);
         }
       }
+
     }
   }
 }
